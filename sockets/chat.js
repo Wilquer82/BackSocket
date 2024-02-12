01739ec29ea14c6cf0331c13162b3749ff4d4819
wilquer82 <wilquer.fig@gmail.com>
2024-02-12 16:11:52 -0300

    ajustes nick duplicado

--- sockets/chat.js
+++ sockets/chat.js
@@ -3,11 +3,17 @@ const { saveMessage } = require('../controller/chatController');
 let users = [];
 
 module.exports = (io) => {
+  let users = []; // Certifique-se de que 'users' é uma variável acessível
+
   io.on('connection', socket => {
     socket.on('saveNickname', nickName => {
-      users.push(nickName);
-      io.emit('usersOn', users);
-      })
+      if (!users.includes(nickName)) { // Verifique se o nome de usuário já existe
+        users.push(nickName);
+        io.emit('usersOn', users);
+      } else {
+        socket.emit('duplicateNickname', nickName); // Emita um evento se o nome de usuário for duplicado
+      }
+    });
       socket.on('message', data => {
         saveMessage(data);
         io.emit('message', data);
