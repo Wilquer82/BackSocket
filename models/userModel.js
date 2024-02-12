1ce228c73280d24aeb8abf3245a96ecc8accc7b0
wilquer82 <wilquer.fig@gmail.com>
2024-02-12 17:41:49 -0300

    ajustes banco

--- models/userModel.js
+++ models/userModel.js
@@ -3,18 +3,18 @@ const connection = require('./connection');
 
 const saveUser = async (nickName) => {
   const db = await connection();
-  const newUser = await db.collection('Users').insertOne({ nickName });
+  const newUser = await db.collection('users').insertOne({ nickName });
   return newUser;
 };
 
 const getUsers = async () => {
   const db = await connection();
-  return db.collection('Users').find().toArray();
+  return db.collection('users').find().toArray();
 };
 
 const deleteUser = async (nickName) => {
   const db = await connection();
-  return db.collection('Users').deleteOne({ nickName });
+  return db.collection('users').deleteOne({ nickName });
 };
 
 module.exports = {
--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const app = express();
 const { getMessages } = require('./controller/chatController');
+const { saveUser, getUsers, deleteUser } = require('./models/userModel');
 const server = require('http').createServer(app);
 const cors = require('cors')
 const PORT = process.env.PORT || 8080;
 
 const options = {
-    methods: ['GET', 'POST'],
+    methods: ['GET', 'POST', 'DELETE'],
     origin:'*', 
     credentials: true,  
     optionSuccessStatus: 200,
   }
+
+  
 
 app.use(cors(options));
 
@@ -24,6 +27,25 @@ require('./sockets/chat')(io);
 
 app.get('/', getMessages); 
 app.use('/get', getMessages);
+
+// Rotas para usuários
+app.get('/users', async (req, res) => {
+  const users = await getUsers();
+  res.json(users);
+});
+
+app.post('/users', async (req, res) => {
+  const { nickName } = req.body;
+  const newUser = await saveUser(nickName);
+  res.json(newUser);
+});
+
+app.delete('/users/:nickName', async (req, res) => {
+  const { nickName } = req.params;
+  await deleteUser(nickName);
+  res.json({ message: 'Usuário deletado com sucesso' });
+});
+
 
 server.listen(PORT, () => {
   console.log(`Porta ${PORT}`);
