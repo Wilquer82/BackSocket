80a36e8e20788caab0e91b5b15d1f4d4c8ee43be
wilquer82 <wilquer.fig@gmail.com>
2024-02-12 17:09:48 -0300

    ajustes dupl

--- sockets/chat.js
+++ sockets/chat.js
@@ -5,11 +5,11 @@ module.exports = (io) => {
 
   io.on('connection', socket => {
     socket.on('saveNickname', nickName => {
-      if (!users.includes(nickName)) { // Verifique se o nome de usuário já existe
+      if (!users.includes(nickName)) {
         users.push(nickName);
         io.emit('usersOn', users);
       } else {
-        socket.emit('duplicateNickname', nickName); // Emita um evento se o nome de usuário for duplicado
+        socket.emit('duplicateNickname', nickName); // Emita o evento apenas para o cliente atual
       }
     });
       socket.on('message', data => {
