2fe85186a931dd35a95647e6f72b7d3f8e5f10ef
wilquer82 <wilquer.fig@gmail.com>
2024-02-12 17:00:37 -0300

    ajustes

--- sockets/chat.js
+++ sockets/chat.js
@@ -4,10 +4,9 @@ module.exports = (io) => {
   let users = []; // Certifique-se de que 'users' é uma variável acessível
 
   io.on('connection', socket => {
-    socket.on('saveNickname',async(nickName) => {
+    socket.on('saveNickname', nickName => {
       if (!users.includes(nickName)) { // Verifique se o nome de usuário já existe
         users.push(nickName);
-        await saveUser(nickName); // Salve o usuário no banco de dados
         io.emit('usersOn', users);
       } else {
         socket.emit('duplicateNickname', nickName); // Emita um evento se o nome de usuário for duplicado
@@ -16,16 +15,16 @@ module.exports = (io) => {
       socket.on('message', data => {
         saveMessage(data);
         io.emit('message', data);
-      })
+       })
     socket.on('disconnect', nickName => {
       let usersActual = users.filter((item) => item !== nickName);
       console.log(usersActual);
       io.emit('usersOn', usersActual);
     })
-    socket.on('userExit', async (nickName) => {
+    socket.on('userExit', nickName => { // Ouça o evento 'userExit'
       users = users.filter((item) => item !== nickName);
-      await deleteUser(nickName); // Remova o usuário do banco de dados
       io.emit('usersOn', users);
     });
+
   })
 }
